6cae0bc1db2976738f588c554cf50bbb3d8efc97
rickalfa <rb__samael_@hotmail.es>
2025-07-24 00:08:46 -0400

    se corrigio error de boton wsp

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -617,12 +617,14 @@
 </style>
 
 
-
+<!--- BOTON whatsapp   -->
+<script>
 
        $('#button-w').whatsappChatSupport({
             defaultMsg : '',
         });
-    </script>
+
+</script>
 
      <!-- Bootstrap core JS-->
      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
